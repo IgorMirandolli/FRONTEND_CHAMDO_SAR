e9a58da98f9cb2c8cf6050d113378ef4b21bed29
eduepagri <eduardocarvalho@epagri.sc.gov.br>
2025-04-11 17:49:54 -0300

    fazendo salvamento

--- src/pages/Chamados/FormAbrirChamado.vue
+++ src/pages/Chamados/FormAbrirChamado.vue
@@ -8,19 +8,75 @@
         
         <q-form @submit.prevent="onSubmit">
           <div class="q-mx-auto q-pa-md q-gutter-y-sm ">          
-            <q-input
-              class="q-pb-md"
-              rounded outlined
-              dense
-              color="amber-9"
-              v-model="form.name"
-              label="Colaborador"
-              type="text"
+
+            <q-select
+                class="q-pb-none"
+                rounded outlined
+                dense
+                color="amber-9"
+                use-input
+                hide-selected
+                fill-input
+                input-debounce="0"
+                :options="colaboradores"
+                v-model="idColaborador"
+                label="Colaborador"
+                @filter="filterFn"
+                emit-value
+                map-options                
             >
               <template v-slot:prepend>
-                <q-icon name="mdi-account-outline" />
+                <q-icon name="map" />
               </template>
-            </q-input>              
+  
+              <template v-slot:no-option>
+                <q-item>
+                  <q-item-section class="text-grey">
+                    Nenhuma opção encontrada
+                  </q-item-section>
+                </q-item>
+              </template>
+            </q-select>    
+
+            <q-select        
+              class="q-pb-sm"      
+              v-model="selectedCategory"
+              :options="categorias"
+              option-label="ds_categoria"
+              option-value="id_categoria"
+              emit-value
+              map-options
+              label="Categoria"  
+              dense      
+              rounded outlined
+              color="amber-9"
+              @update:model-value="handleCategotyChange"
+            >
+              <template v-slot:prepend>
+                  <q-icon name="mdi-checkbox-marked-outline" />
+                </template>
+            </q-select>
+
+            <!-- SELECT DINÂMICO DE SUBCATEGORIAS -->
+            <q-select
+              v-if="subCategorias.length"
+              v-model="selectedSubCategory"
+              :options="subCategorias"
+              option-label="ds_categoria"
+              option-value="id_categoria"
+              emit-value
+              map-options
+              label="Subcategoria"
+              dense
+              rounded
+              outlined
+              color="amber-9"
+              class="q-mt-sm"
+            >
+              <template v-slot:prepend>
+                <q-icon name="subdirectory_arrow_right" />
+              </template>
+            </q-select>
 
             <q-editor
               v-model="form.editor"
@@ -57,68 +113,111 @@
                 <q-icon name="crop_original" />
               </template>
   
-            </q-file>
-
-              
+            </q-file>              
     
-              <q-btn label="Salvar" type="submit"  color="primary" class="full-width"  rounded />
-    
-              <q-btn label="Voltar" color="primary" class="full-width" rounded flat :to="{ name: 'chamados'}" />
+            <q-btn label="Salvar" type="submit"  color="primary" class="full-width"  rounded />
+  
+            <q-btn label="Voltar" color="primary" class="full-width" rounded flat :to="{ name: 'chamados'}" />
     
           </div>
     
         </q-form>
       </div>
-    </q-card>
-    
-    <q-dialog v-model="persistent" persistent transition-show="scale" transition-hide="scale">
-      <q-card class="bg-teal text-white" style="width: 300px">
-        <q-card-section>
-          <div class="text-h6">??????????:</div>
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          ????????????????
-        </q-card-section>
-
-        <q-card-actions align="right" class="bg-white text-teal">
-          <q-btn flat label="Não" color="primary" v-close-popup :to="{ name: 'properties'}"/>
-          <q-btn flat label="Sim" v-close-popup @click="rent()" />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
+    </q-card>    
     
 
   </q-page>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import { useQuasar } from 'quasar'
+import { useRoute } from 'vue-router'
+import { api } from 'boot/axios'
 import useNotify from 'src/composables/UseNotify'
-// import { columnsCategorias } from 'src/composables/UseTable'
-// import { api } from 'boot/axios'
 
 const { notifyError } = useNotify()
 const categorias = ref([])
+const subCategorias = ref([])
+const selectedCategory = ref()
+const selectedSubCategory = ref()
 const $q = useQuasar()
-const form = ref({})
-
-onMounted(() => {
-  getCategorias()
+const form = ref({
+  editor: '',
 })
+const route = useRoute()
+const colaboradores = ref([])
+const idColaborador = ref(null)
+const colaborador = ref(null)
+const stringOptions = ref([]) 
+const files= ref([])
+
+
+onMounted(async() => {
+  categorias.value = await getCategorias(route.params.id)  
+  getColaboradores()    
+  idColaborador.value = $q.localStorage.getItem('cd_colaborador')  
+})
+
+const filterFn = (val, update, abort) => {
+  if (!val) {
+    update(() => {
+      colaboradores.value = stringOptions.value
+    })
+    return
+  }
+
+  const normalize = str => str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase()
+
+  update(() => {
+    const needle = normalize(val)
+    colaboradores.value = stringOptions.value.filter(v =>
+      normalize(v.label).includes(needle)
+    )
+  })
+}
+
+const getColaboradores = async () => {
+  try {
+    const { data } = await api.get('colaboradores/')
+
+    const options = data.map(colab => ({
+      value: colab.CD_COLABORADOR,
+      label: colab.NM_COMPLETO_COLAB
+    }))
+
+    colaboradores.value = options
+    stringOptions.value = options
+  } catch (error) {
+    notifyError(error.message)
+  }
+}
+
+const handleCategotyChange = async (id_categoria) => {
+  selectedSubCategory.value = null
+  subCategorias.value = await getCategorias(id_categoria)
+}
 
 const getCategorias = async (id) => {
   try {
     $q.loading.show()
-    // const { data } = await api.get('categoria-master/')
-    // categorias.value = data
-    console.log(categorias.value)
+    const { data } = await api.get('categoria-superior/'+ id)
     $q.loading.hide()
+    return data    
   } catch (error) {
     notifyError(error.message)
     $q.loading.hide()
   }
 }
+
+const onSubmit = async() => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+
 
 </script>
--- src/pages/Chamados/ListCategoriasChamado.vue
+++ src/pages/Chamados/ListCategoriasChamado.vue
@@ -1,9 +1,10 @@
 <template>
-    <q-page padding >
-      <div class="row justify-center">
+  <q-page padding >
+    
+    <div class="row justify-center">
 
       <q-table
-        :rows="categorias"
+        :rows="listCategorias"
         :columns="columnsCategorias"
         row-key="id_categoria"
         grid
@@ -26,7 +27,7 @@
                   class="cursor-pointer"
                   :src="props.row.img_url"
                   :ratio="5/3"
-                  @click="abrirChamado(props.row.id_categoria)"
+                  @click="abrirChamado(props.row)"
                 >
                   <div class="absolute-bottom text-subtitle2 text-center">
                     {{ props.row.ds_categoria }}
@@ -38,13 +39,137 @@
         </transition>
       </template>
 
-    </q-table>
+      </q-table>
 
-      </div>
-      <br>
+    </div>
 
-    </q-page>
+    <q-dialog v-model="showPreviewDialog" persistent>      
 
+      <q-card class="q-mx-auto q-mt-md " style="min-width: 700px; border-radius: 20px"> 
+        <div class="container q-mx-auto q-mt-md" style="max-width: 500px;">
+
+          <h4 class="text-h6 text-primary text-center q-my-none">{{ nameCategoria }}</h4>
+          
+          <q-form @submit.prevent="onSubmit">
+            <div class="q-mx-auto q-pa-md q-gutter-y-sm ">          
+
+              <q-select
+                  class="q-pb-none"
+                  rounded outlined
+                  dense
+                  color="amber-9"
+                  use-input
+                  hide-selected
+                  fill-input
+                  input-debounce="0"
+                  :options="colaboradores"
+                  v-model="idColaborador"
+                  label="Colaborador"
+                  @filter="filterFn"
+                  emit-value
+                  map-options                
+              >
+                <template v-slot:prepend>
+                  <q-icon name="map" />
+                </template>
+    
+                <template v-slot:no-option>
+                  <q-item>
+                    <q-item-section class="text-grey">
+                      Nenhuma opção encontrada
+                    </q-item-section>
+                  </q-item>
+                </template>
+              </q-select>    
+
+              <q-select        
+                class="q-pb-sm"      
+                v-model="selectedCategory"
+                :options="categorias"
+                option-label="ds_categoria"
+                option-value="id_categoria"
+                emit-value
+                map-options
+                label="Categoria"  
+                dense      
+                rounded outlined
+                color="amber-9"
+                @update:model-value="handleCategotyChange"
+              >
+                <template v-slot:prepend>
+                    <q-icon name="mdi-checkbox-marked-outline" />
+                  </template>
+              </q-select>
+
+              <!-- SELECT DINÂMICO DE SUBCATEGORIAS -->
+              <q-select
+                v-if="subCategorias.length"
+                v-model="selectedSubCategory"
+                :options="subCategorias"
+                option-label="ds_categoria"
+                option-value="id_categoria"
+                emit-value
+                map-options
+                label="Subcategoria"
+                dense
+                rounded
+                outlined
+                color="amber-9"
+                class="q-mt-sm"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="subdirectory_arrow_right" />
+                </template>
+              </q-select>
+
+              <q-editor
+                v-model="form.editor"
+                rounded
+                outlined
+                dense
+                color="amber-9"
+                class="q-mb-md bg-blue-grey-2"
+                min-height="10rem"
+                :content-style="{
+                  backgroundColor: 'transparent',
+                  padding: '8px',
+                  fontSize: '14px'
+                }"
+                :toolbar="[
+                  ['bold', 'italic', 'underline'],
+                  ['unordered', 'ordered'],
+                  ['link']
+                ]"
+              />
+      
+              <q-file              
+                color="amber-9"
+                v-model="files"
+                label="Anexos"
+                rounded outlined
+                dense
+                multiple
+                append
+                use-chips
+                accept="*"
+                >
+                <template v-slot:prepend>
+                  <q-icon name="mdi-attachment" />
+                </template>
+    
+              </q-file>              
+      
+              <q-btn label="Salvar" type="submit"  color="primary" class="full-width" v-close-popup rounded />
+    
+              <q-btn label="Sair" color="primary" class="full-width" rounded flat v-close-popup @click="cleanDialog" />
+      
+            </div>
+      
+          </q-form>
+        </div>
+      </q-card>
+    </q-dialog>
+  </q-page>
 </template>
 
 <script setup>
@@ -56,37 +181,59 @@ import { api } from 'boot/axios'
 import { useRouter } from 'vue-router'
 
 const { notifyError } = useNotify()
-const categorias = ref([])
+const listCategorias = ref([])
 const $q = useQuasar()
 const router = useRouter()
 
+///////////////////////////////////
+const showPreviewDialog = ref(false)
+const categorias = ref([])
+const subCategorias = ref([])
+const selectedCategory = ref()
+const selectedSubCategory = ref()
+const form = ref({
+  editor: '',
+})
+
+const colaboradores = ref([])
+const idColaborador = ref(null)
+const nameCategoria = ref(null)
+const colaborador = ref(null)
+const stringOptions = ref([]) 
+const files= ref([])
+
+
 onMounted(async () => {
+  $q.loading.show()
   const validacao = {
-    token: '111352535829370743381207707859',
+    token: '28211958347828700123580312950',
     cd_colaborador: '15948'
     // token: route.query.dsTokenAppUsuario,
     // cd_colaborador: route.query.cdUsuario
   }
-  try {    
+  try {        
     const response = await api.post('validateToken/', validacao)    
     if (response.data.msg === 'Não autorizado') {
       window.location = response.data.url
     } else {
-      getCategorias()
-      const { data } = await api.get('colaborador/'+ validacao.cd_colaborador )          
-      $q.localStorage.set('cd_colaborador', data[0].cd_colaborador)
-      $q.localStorage.set('cd_unorg', data[0].cd_unorg)
+      getListCategorias()
+      const { data } = await api.get('colaborador/'+ validacao.cd_colaborador )                
+      $q.localStorage.set('cd_colaborador', data[0].CD_COLABORADOR)
+      $q.localStorage.set('nm_completo_colab', data[0].NM_COMPLETO_COLAB)
+      $q.localStorage.set('cd_unorg', data[0].CD_UNORG)
     }
   } catch (error) {
+    $q.loading.hide()
     console.log(error)
   }
+  $q.loading.hide()
 })
 
-const getCategorias = async (id) => {
+const getListCategorias = async (id) => {
   try {
     $q.loading.show()
     const { data } = await api.get('categoria-master/')
-    categorias.value = data    
+    listCategorias.value = data    
     $q.loading.hide()
   } catch (error) {
     notifyError(error.message)
@@ -94,8 +241,78 @@ const getCategorias = async (id) => {
   }
 }
 
-const abrirChamado = (id) => {  
-  router.push({ name: 'abrirChamado', params: { id: id } })
+const abrirChamado = async (props) => {    
+  showPreviewDialog.value = true
+  categorias.value = await getCategorias(props.id_categoria)    
+  nameCategoria.value = props.ds_categoria
+  getColaboradores()    
+  idColaborador.value = $q.localStorage.getItem('cd_colaborador')  
+}
+
+const filterFn = (val, update, abort) => {
+  if (!val) {
+    update(() => {
+      colaboradores.value = stringOptions.value
+    })
+    return
+  }
+
+  const normalize = str => str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase()
+
+  update(() => {
+    const needle = normalize(val)
+    colaboradores.value = stringOptions.value.filter(v =>
+      normalize(v.label).includes(needle)
+    )
+  })
+}
+
+const getColaboradores = async () => {
+  try {
+    const { data } = await api.get('colaboradores/')
+
+    const options = data.map(colab => ({
+      value: colab.CD_COLABORADOR,
+      label: colab.NM_COMPLETO_COLAB
+    }))
+
+    colaboradores.value = options
+    stringOptions.value = options
+  } catch (error) {
+    notifyError(error.message)
+  }
+}
+
+const handleCategotyChange = async (id_categoria) => {
+  selectedSubCategory.value = null
+  subCategorias.value = await getCategorias(id_categoria)
+}
+
+const getCategorias = async (id) => {
+  try {
+    $q.loading.show()
+    const { data } = await api.get('categoria-superior/'+ id)
+    $q.loading.hide()
+    return data    
+  } catch (error) {
+    notifyError(error.message)
+    $q.loading.hide()
+  }
+}
+
+const cleanDialog = async() => {
+  selectedCategory.value = null
+  subCategorias.value.length = 0
+  files.value = null
+  form.value.editor = ''
+}
+
+const onSubmit = async() => {
+  try {
+    
+  } catch (error) {
+    
+  }
 }
 
 </script>
